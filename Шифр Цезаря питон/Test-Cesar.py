815fa851b255dce8a3cd5136988502a0d98e0c3f
Stasy-Nabieva <nabieva-nasty@mail.ru>
2025-04-13 20:41:11 +0300

    Update Test-Cesar.py

--- Шифр Цезаря питон/Test-Cesar.py
+++ Шифр Цезаря питон/Test-Cesar.py
@@ -1,21 +1,27 @@
 import unittest
 from Cesar import encrypt
+from Cesar import decrypt
 
-class TestCaesarCipher(unittest.TestCase):
+class TestEncryptionDecryption(unittest.TestCase):
 
     def test_encryption(self):
         self.assertEqual(encrypt("Hello, World!", 3), "Khoor, Zruog!")
-        self.assertEqual(encrypt("abc", 2), "cde")
-        self.assertEqual(encrypt("XYZ", 3), "ABC")
+        self.assertEqual(encrypt("abc", 1), "bcd")
+        self.assertEqual(encrypt("xyz", 3), "abc")
+      
+    
+    def test_decryption(self):
+        self.assertEqual(decrypt("Khoor, Zruog!", 3), "Hello, World!")
+        self.assertEqual(decrypt("bcd", 1), "abc")
+        self.assertEqual(decrypt("abc", 3), "xyz")
 
     def test_no_shift(self):
-        self.assertEqual(encrypt("Hello, World!", 0), "Hello, World!")
-        
-    def test_non_alpha(self):
-        self.assertEqual(encrypt("1234 @#!", 5), "1234 @#!")
-        
-    def test_empty_string(self):
-        self.assertEqual(encrypt("", 3), "")    
+        self.assertEqual(encrypt("No Shift", 0), "No Shift")
+        self.assertEqual(decrypt("No Shift", 0), "No Shift")
+
+    def test_non_alpha_characters(self):
+        self.assertEqual(encrypt("Hello, World 123!", 5), "Mjqqt, Btwqi 123!")
+        self.assertEqual(decrypt("Mjqqt, Btwqi 123!", 5), "Hello, World 123!")
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main() 
